d850d5c09e10f49d91a5c552d24ef26aeb04b1c3
Ashcon Mohseninia <ashconm@outlook.com>
2023-01-27 20:28:59 +0100

    Working BT

--- pc_sw/daugher_pcb_comm/src/agw/bluetooth_manager.rs
+++ pc_sw/daugher_pcb_comm/src/agw/bluetooth_manager.rs
@@ -4,7 +4,8 @@ use bluer::{Adapter, AdapterEvent, Address, Device, DeviceEvent};
 use dbus::arg::{PropMap, RefArg};
 use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
 use dbus::{blocking::Connection, channel::MatchingReceiver, message::MatchRule, Error, Message};
-use std::sync::mpsc::Sender;
+use tokio::{task, time};
+use std::sync::mpsc::{Sender, self};
 use std::sync::{Arc, RwLock};
 use std::{future, time::Duration};
 use tokio::sync::futures;
@@ -14,9 +15,16 @@ const PATH: &str = "/org/bluez";
 const INTERFACE: &str = "org.bluez.Adapter1";
 const ADAPTER: &str = "hci0";
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum BtCommand {
+    Next,
+    Prev
+}
+
+#[derive(Debug, Clone)]
 pub struct BluetoothManager {
     connected_device: Arc<RwLock<Option<String>>>,
+    sender: mpsc::Sender<BtCommand>
 }
 
 impl BluetoothManager {
@@ -24,6 +32,7 @@ impl BluetoothManager {
         let connected_device = Arc::new(RwLock::new(None));
         let connected_device_t = connected_device.clone();
 
+        let (sender_bt, receiver_bt) = mpsc::channel::<BtCommand>();
         std::thread::spawn(move || {
             let BT_IDLE_STATE: AudioPageState = AudioPageState {
                 header_text: IcText {
@@ -42,22 +51,21 @@ impl BluetoothManager {
                 .enable_all()
                 .build()
                 .unwrap();
-            let adapter: Adapter;
             log::debug!("Waiting for bluetooth");
-            loop {
-                if let Ok(a) = rt.block_on(async {
-                    let session = bluer::Session::new().await.unwrap();
-                    session.default_adapter().await
-                }) {
-                    adapter = a;
-                    break;
+            let adapter = rt.block_on(async {
+                let session = bluer::Session::new().await.unwrap();
+                loop {
+                    if let Ok(a) = session.default_adapter().await {
+                        return a;
+                    }
                 }
-            }
+            });
             log::info!("Bluetooth now up!");
             sender.send(AgwCommand::SetAudioPage(BT_IDLE_STATE.clone()));
             let connection = Connection::new_system().unwrap();
             let mut rule = MatchRule::new();
             loop {
+                let mut act = false;
                 let mut dev_name: Option<String> = None;
                 let mut dev: Option<Device> = None;
                 rt.block_on(async {
@@ -121,6 +129,7 @@ impl BluetoothManager {
                         Duration::from_millis(1000),
                     );
                     if let Ok(meta) = proxy.get::<PropMap>("org.bluez.MediaPlayer1", "Track") {
+                        act = true;
                         if let Some(track) = meta.get_key_value("Title") {
                             let track_name = track.1.as_str().unwrap();
                             let mut tx_text = Some(String::from("Not playing"));
@@ -146,16 +155,35 @@ impl BluetoothManager {
                                 }
                             }
                         }
+                        if let Ok(cmd) = receiver_bt.try_recv() {
+                            let _: Result<(), Error> = match cmd {
+                                BtCommand::Next =>  proxy.method_call("org.bluez.MediaPlayer1", "Next", ()),
+                                BtCommand::Prev =>  proxy.method_call("org.bluez.MediaPlayer1", "Previous", ())
+                            };
+                            println!("Bt command {:?}", cmd);
+                        }
                     }
                 }
-                std::thread::sleep(std::time::Duration::from_millis(1000));
+                if act {
+                    std::thread::sleep(std::time::Duration::from_millis(250));
+                } else {
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
+                }
             }
         });
 
-        Self { connected_device }
+        Self { 
+            connected_device,
+            sender: sender_bt
+        }
     }
 
     pub fn connected_device(&self) -> Option<String> {
         self.connected_device.read().unwrap().clone()
     }
+
+    pub fn send_media_control(&self, cmd: BtCommand) {
+        self.sender.send(cmd);
+    }
+
 }
